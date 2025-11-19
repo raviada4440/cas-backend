223a993151dfe375ec76ce131b12d8fe97d030fc
Ravi Ada <raviada@dobeyond.com>
2025-11-19 14:39:24 -0600

    update lint rule to supress warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,6 @@ module.exports = {
       },
     ],
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 }
